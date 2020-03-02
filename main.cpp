8d6711485163ef1b29c7cd7e6ebfed38db59eb56
aslam29 <61522091+aslam29@users.noreply.github.com>
2020-03-02 15:07:27 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,87 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <limits>
+
 class Fish {
 private:
     bool alive;
     bool hungry;
-    string name;
+    std::string name;
 
 public:
     // Default Constructor
     Fish() {
-        alive = true;
-        name  = "Fishy"
+        alive  = true;
+        hungry = false;
+        name   = "Fishy";
     }
 
     // Constructor
-    Fish(bool x, string y) {
-        alive = x;
-        name  = y;
+    Fish(bool x, bool y, std::string z) {
+        alive  = x;
+        hungry = y;
+        name   = z;
     }
 
     // Return fish's life status
-    bool isAlive() {
-        return alive;
-    }
+    bool isAlive() { return alive; }
 
     // Return fish's hunger status
-    bool isHungry() {
-        return hungry;
-    }
+    bool isHungry() { return hungry; }
 
-    // Print fish's name
-    void printName() {
-        cout << name;
-    }
+    // Return fish's name
+    std::string printName() { return name; }
+
+    // Set fish's life status
+    void setAlive(bool status) { alive = status; return; }
+
+    // Set fish's hunger status
+    void setHungry(bool status) { hungry = status; return; }
 };
 
 int main() {
-    Fish Dennis(true, "Dennis");    // Create my cute fish, Dennis!
+    srand( (unsigned)time(NULL) );   // initialize the sequence of pseudo-random numbers with a random seed
+
+    Fish Dennis(true, false, "Dennis");   // Create my cute fish, Dennis!
 
     // Dennis's life is like a loop. 
-    while(Dennis.isAlive()) {
-        cout << "Hello, I'm " << Dennis.printName() << "!" << endl;
+    while (Dennis.isAlive()) {
+        std::cout << "Hello, I'm " << Dennis.printName() << "!" << std::endl << std::endl;
 
-        for(int i = 0; i < 5; i++) {
-            cout << "..." << endl;
+        // Dennis is chilling until he gets hungry
+        while(!Dennis.isHungry()) {
+            // Randomly chooses a number from 1 to 10 and if it is 2, Dennis is now hungry
+            if ( (rand() % 3) + 1 == 2 ) {
+                Dennis.setHungry(true);
+                break;
+            }
+
+            // Till then, Dennis is just chilling in his tank :^)
+            std::cout << "(chilling...)" << std::endl;
         }
+
+        // Dennis is now hungry :^(
+        std::cout << std::endl << "My hungry..." << std::endl;
+
+        // Time to feed Dennis!
+        std::cout << std::endl << "Press Enter to Feed Dennis" << std::endl;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+
+        // Dennis notices the food
+        std::cout << "!" << std::endl;
+
+        // Time for Dennis to eat!
+        std::cout << std::endl << "(NOM! NOM! NOM! NOM! NOM!)" << std::endl;
+
+        Dennis.setHungry(false);  // Dennis is now full
+
+        // Dennis thanks his owner for the food <3
+        std::cout << std::endl << "Thank you! <3" << std::endl;
+
+        // Continue on to the next day -- repeating Dennis's life over and over again till death
+        std::cout << std::endl << "Press Enter to Continue to the Next Day" << std::endl;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
     }
 }
